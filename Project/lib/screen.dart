26dbbe93b7ef1a5929dfb5c2c9675a0aaf3333a9
VictorTPhan <71913548+VictorTPhan@users.noreply.github.com>
2023-08-11 15:03:01 -0700

    Update screen.dart

--- Project/lib/screen.dart
+++ Project/lib/screen.dart
@@ -18,64 +18,59 @@ class _ScreenPageState extends State<ScreenPage> {
       appBar: AppBar(
         title: Text('${widget.title} Database'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children:[
-          FutureBuilder(
-            future: FirebaseDatabase.instance.ref().child("${widget.title}s").once(),
-            builder: (context, asyncSnapshot){
-              if (asyncSnapshot.hasData){
-                try {
-                  var asyncSnapshotList = asyncSnapshot.data?.snapshot.children.toList();
+      body: FutureBuilder(
+        future: FirebaseDatabase.instance.ref().child("${widget.title}s").once(),
+        builder: (context, asyncSnapshot){
+          if (asyncSnapshot.hasData){
+            try {
+              var asyncSnapshotList = asyncSnapshot.data?.snapshot.children.toList();
 
-                  final postList = List.empty(growable: true);
-                  for (DataSnapshot dataSnapshot in asyncSnapshotList!) {
-                    postList.add(dataSnapshot.key);
-                  }
+              final postList = List.empty(growable: true);
+              for (DataSnapshot dataSnapshot in asyncSnapshotList!) {
+                postList.add(dataSnapshot.key);
+              }
 
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: postList!.length,
-                    itemBuilder: (BuildContext ctx, i)=> Container(
-                      height: 100,
-                      width: 100,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ScreenDetailPage(
-                                    title:postList[i],
-                                    ref: widget.title + "s"
-                                )
-                            ),
-                          );
-                        },
-                        child: Card(
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                          child: Center(
-                            child: Text(
-                                postList[i],
-                                style: TextStyle(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.w500
-                                ),
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: postList!.length,
+                itemBuilder: (BuildContext ctx, i)=> Container(
+                  height: 100,
+                  width: 100,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ScreenDetailPage(
+                                title:postList[i],
+                                ref: widget.title + "s"
                             )
-                          ),
                         ),
+                      );
+                    },
+                    child: Card(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                      child: Center(
+                        child: Text(
+                            postList[i],
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w500
+                            ),
+                        )
                       ),
                     ),
-                  );
-                } catch (Exception) {
-                  return const Text("Error loading data");
-                }
-              }
-              else { // Loading data
-                return const Text("loading...");
-              }
-            },
-          ),
-        ],
+                  ),
+                ),
+              );
+            } catch (Exception) {
+              return const Text("Error loading data");
+            }
+          }
+          else { // Loading data
+            return const Text("loading...");
+          }
+        },
       ),
     );
   }
